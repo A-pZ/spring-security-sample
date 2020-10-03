3fa2914b24eb9ee0914819bf5f4e31d9a7c2723e
A-pZ <g.alpha.pz@gmail.com>
2020-10-03 10:19:40 +0900

    warデプロイに対応

--- src/main/java/com/github/apz/SpringSecuritySampleApplication.java
+++ src/main/java/com/github/apz/SpringSecuritySampleApplication.java
@@ -2,12 +2,19 @@ package com.github.apz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringSecuritySampleApplication {
+public class SpringSecuritySampleApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecuritySampleApplication.class, args);
 	}
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringSecuritySampleApplication.class);
+    }
 
 }
